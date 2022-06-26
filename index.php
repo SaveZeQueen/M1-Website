17aa568074706f5221166bf64fef4d94aa3ed3f5
SaveZeQueen <35947471+SaveZeQueen@users.noreply.github.com>
2022-06-26 10:28:38 -0400

    Version 0.1 - Initial Upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>The Miracle One</title>
+        <link rel="stylesheet" href="style/style.css" />
+        <script src="scripts/script.js" defer></script>
+        <script src="scripts/smoothScroll.js" defer></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+        <div id="TOP"></div>
+        <div id="NavBar">
+            <a href="index.php#AboutBlock" class="current">About</a>
+            <a href="">Portfolio</a>
+            <a href="index.php#TOP"><img src="images/Logo_M1_4.png"/></a>
+            <a href="">Resources</a>
+            <a href="">Booking</a>
+        </div>
+
+        <div id="BannerImg">
+            <div class="Boverlay">
+            <section>
+                <h1>The Miracle One</h1>
+                <blockquote>- Making every memory a miracle -</blockquote>
+            </section>
+            </div>
+        </div>
+
+        <div id="AboutBlock">
+            <div class="photographer"></div>
+            <div class="about">
+                <h1>About me</h1>
+                <div class="divider"></div>
+                <section>
+
+                </section>
+            </div>
+        </div>
+        
+
+    </body>
+</html>
---
+++ scripts/smoothScroll.js
@@ -0,0 +1,41 @@
+// Select all links with hashes
+// @ts-ignore
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      // @ts-ignore
+      var target = $(this.hash);
+      // @ts-ignore
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        // @ts-ignore
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 100, function() {
+          // Callback after animation
+          // Must change focus!
+          // @ts-ignore
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
